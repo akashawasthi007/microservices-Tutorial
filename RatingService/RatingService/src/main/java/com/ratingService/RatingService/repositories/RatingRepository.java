28b037bc201234dd5cc5afe1eb484a7f2fd4f634
Akash Awasthi <akashawasthi076@gmail.com>
2025-08-18 18:58:04 +0530

    test

--- RatingService/RatingService/src/main/java/com/ratingService/RatingService/repositories/RatingRepository.java
+++ RatingService/RatingService/src/main/java/com/ratingService/RatingService/repositories/RatingRepository.java
@@ -1,9 +1,14 @@
 package com.ratingService.RatingService.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ratingService.RatingService.entities.Rating;
 
-public interface RatingRepository extends JpaRepository<Rating,String> {
+public interface RatingRepository extends JpaRepository<Rating,Long> {
+
+	List<Rating> findAllRatingsByHotelId(String id);
+	List<Rating> findAllRatingsByUserId(String id);
 
 }
